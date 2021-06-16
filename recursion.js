1ead2ef47dcec47214803d1dbabafa612695ea5b
Michael Schienbein <moomba0@gmail.com>
2021-06-16 14:49:45 +0700

    added recursive binary search

--- recursion.js
+++ recursion.js
@@ -62,30 +62,20 @@ function gatherStrings(obj) {
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
-
-  let leftIdx = 0;
-  let rightIdx = arr.length - 1;
-
-  while (leftIdx <= rightIdx) {
-    // find the middle value
-    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-    let middleVal = arr[middleIdx];
-
-    if (middleVal < val) {
-      // middleVal is too small, look at the right half
-      leftIdx = middleIdx + 1;
-    } else if (middleVal > val) {
-      // middleVal is too large, look at the left half
-      rightIdx = middleIdx - 1;
-    } else {
-      // we found our value!
-      return middleIdx;
-    }
+function binarySearch(arr, val, leftIdx = 0, rightIdx = arr.length - 1) {
+  if (leftIdx > rightIdx) {
+    return -1;
   }
+  let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+  let middleVal = arr[middleIdx];
+  if (middleVal === val) {
+    return middleIdx;
+  }
+  if (middleVal > val) {
+    return binarySearch(arr, val, leftIdx, middleIdx - 1);
+  }
+  return binarySearch(arr, val, middleIdx + 1, rightIdx);
 
-  // left and right pointers crossed, val isn't in arr
-  return -1;
 }
 
 module.exports = {
